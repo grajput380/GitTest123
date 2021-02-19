a72aeffd1d7b1fcdda20d97c732bffb343fbc4d3
Gaurav Rajput <gaurav.rajput@globallogic.com>
2021-02-19 21:10:40 +0530

    forth commit

--- src/test/java/com/gittest123/testPackage/TestClass.java
+++ src/test/java/com/gittest123/testPackage/TestClass.java
@@ -6,5 +6,6 @@ public class TestClass {
 		System.out.println("first commit");
 		System.out.println("second commit");
 		System.out.println("third commit");
+		System.out.println("forth commit");
 	}
 }
