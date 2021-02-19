fb9aa3f10e11074517f860c46ffe2309bf0e439d
Gaurav Rajput <gaurav.rajput@globallogic.com>
2021-02-19 12:58:41 +0530

    Reverted second commit
    This reverts commit f6b27bb92a83f2594988917657c93527ffcb8807.

--- src/test/java/com/gittest123/testPackage/TestClass.java
+++ src/test/java/com/gittest123/testPackage/TestClass.java
@@ -4,8 +4,5 @@ public class TestClass {
 
 	public static void main(String[] args) {
 		System.out.println("first commit");
-		System.out.println("second commit");
-		System.out.println("third commit");
-		System.out.println("fourth commit");
 	}
 }
